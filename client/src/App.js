56152215da3bef69639685ee111ad0ded292bb40
dpfflqjd <dpfflqhd@naver.com>
2021-01-01 23:14:29 +0900

    Add Get Customers REST API Module

--- client/src/App.js
+++ client/src/App.js
@@ -19,35 +19,25 @@ const styles = {
   }
 }
 
-const customers = [
-  {
-    'id': 1,
-    'image': 'https://placeimg.com/64/64/1',
-    'name': '홍길동',
-    'birthday': '940812',
-    'gender': '남자',
-    'job': '대학생'
-  },
-
-  {
-    'id': 2,
-    'image': 'https://placeimg.com/64/64/2',
-    'name': '김승환',
-    'birthday': '141414',
-    'gender': '남자',
-    'job': '프로그래머'
-  },
-
-  {
-    'id': 3,
-    'image': 'https://placeimg.com/64/64/3',
-    'name': '김승호',
-    'birthday': '114254',
-    'gender': '남자',
-    'job': '오우오우'
-  }
-]
 class App extends Component{
+
+  state = {
+    customers: ""
+  }
+
+  componentDidMount(){
+    this.callApi()
+    .then(res => this.setState({customers: res}))
+    .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+
   render() {
     const { classes } = this.props;
     return (
@@ -64,10 +54,8 @@ class App extends Component{
             </TableRow>
           </TableHead>
           <TableBody>
-  
-  
-            {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />) })}
-  
+            {this.state.customers ? this.state.customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />);
+            }) : ""}
           </TableBody>
         </Table>
       </Paper>
